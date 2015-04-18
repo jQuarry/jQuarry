db537a91ad48d03eed85f75cb97512ba3b1ac14a
Giovanni Carvelli <giovannircarvelli@gmail.com>
2015-04-18 14:56:04 -0400

    Added file for game code and randomly selected subtitles

---
+++ js/jquarry.js
@@ -0,0 +1,20 @@
+
+var Game = {};
+
+Game.launch = function() {
+    var version = 1.0;
+    var date = new Date();
+
+
+    // choose a random subtitle for the page
+    var subs = [
+    'The #1 Javascript-Powered Web Technology Mining Game!'];
+    Game.subtitle = subs[Math.floor(Math.random() * subs.length)];
+
+};
+
+Game.launch();
+
+$('document').ready(function() {
+    $('#subtitle').html(Game.subtitle);
+});
