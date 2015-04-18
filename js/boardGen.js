c71b29e7391c1bdd6655ee390d4c6f032b100ce8
Dakota Williams <drw9888@rit.edu>
2015-04-18 16:29:10 -0400

    very images, much cool

--- js/boardGen.js
+++ js/boardGen.js
@@ -1,14 +1,20 @@
 
-(function genBoard(n) {
+imgs = ["404.jpg", "asp.png", "computer-hacker.jpg", "django.png", "doge.png",
+		"jquery.gif", "php.jpg", "ruby-on-rails.png"];
+
+$('#mybutton69').on('click', function genBoard() {
+	var n = 4;
 	var place = $("#board");
-	place.append('<table id="boardtable" border="1" stlye="align: center;"></table>');
+	place.empty();
+	place.append('<table id="boardtable" border="1"></table>');
 	place = $('#boardtable');
 	for(var i = 0; i < n; i++) {
 		var row = $("<tr></tr>");
 		place.append(row);
 		for(var j = 0; j < n; j++) {
-			row.append("<td>cell (" + i + ", " + j +")</td>");
+			var rand = Math.floor(Math.random() * 7);
+			row.append("<td><img src='img/" + imgs[rand] +"' width=200 height=200/></td>");
 		}
 	}
-})(4);
+});
 
